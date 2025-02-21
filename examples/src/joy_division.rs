32493063164895267909876b48502eb456aadfcd
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-02-21 22:48:27 +0100

    draw details: change base type i32 -> f32

--- examples/src/joy_division.rs
+++ examples/src/joy_division.rs
@@ -5,11 +5,13 @@ use rand::{
 };
 use std::{path::Path, time::Instant};
 use utils::{Line, Point2};
+use itertools;
+use itertools_num::linspace;
 
 fn main() {
     let start = Instant::now();
-    let mut imgbuf = ImageBuffer::new(500, 500);
-    let step: i32 = 50;
+    let mut imgbuf = ImageBuffer::new(50, 50);
+    let step: f32 = 5.;
     println!(
         "Juggling pixels ({}x{})...\nStep:{}.",
         imgbuf.width(),
@@ -31,29 +33,30 @@ fn main() {
 
     // Iterate over the coordinates and pixels of the image
     // IMPORTANT STEP: it defines the shape of curves in the image 
-    let mut horiz: Vec<i32> = imgbuf
+    let mut horiz: Vec<f32> = imgbuf
         .clone()
         .enumerate_pixels_mut()
-        .step_by(step as usize - 1)
-        .filter(|(_, y, _)| *y as i32 % step == 0)
-        .map(|(x, _, _)| x as i32)
-        .take_while(|x| *x <= (imgbuf.width() as i32))
+        .step_by(step as usize)
+        .filter(|(_, y, _)| *y as f32 % step == 0.0)
+        .map(|(x, _, _)| x as f32)
+        .take_while(|x| *x <= (imgbuf.width() as f32))
         .collect();
-        let mut vert: Vec<i32> = imgbuf
+        let mut vert: Vec<f32> = imgbuf
         .clone()
         .enumerate_pixels_mut()
-        .step_by(step as usize - 1)
-        .map(|(_, y, _)| y as i32)
-        .take_while(|y| *y <= (imgbuf.height() as i32))
+        .step_by(step as usize)
+        .map(|(_, y, _)| y as f32)
+        .take_while(|y| *y <= (imgbuf.height() as f32))
         .collect();
     // Dedup vector of y, because the image iterator takes pixel/per iteration
-    vert.dedup_by(|a, b| a == b);
+    // vert.dedup_by(|a, b| a == b);
     // horiz.dedup_by(|a, b| a == b);
+    println!("{:?}", horiz);
     print!("{:?}", vert);
 
     let mut lines: Vec<Line> = Vec::new();
     let rng = thread_rng();
-    let range = Uniform::new_inclusive(0, 10);
+    let range = Uniform::new_inclusive(0., 20.);
     for &i in vert.iter().step_by(step as usize) {
         let mut line = Vec::new();
         for j in horiz.iter().step_by(step as usize) {
@@ -61,7 +64,7 @@ fn main() {
             let mut r = range.sample_iter(rng).next().unwrap();
 
             // array bounds corner case guard
-            let new_y = if i + r < imgbuf.height() as i32 {
+            let new_y = if i + r < imgbuf.height() as f32 {
                 i + r
             } else {
                 i
@@ -74,7 +77,7 @@ fn main() {
     println!("\n{} lines", lines.len());
 
     for l in lines.iter() {
-        let p0 = &l.points[0];
+        let _p0 = &l.points[0];
         for (i, p) in l.points.iter().enumerate() {
             let p_start = if i < 1 { 0 } else { i - 1 };
             utils::line(&mut imgbuf, &l.points[p_start], p);
@@ -86,10 +89,10 @@ fn main() {
     println!("Time taken: {:?}", start.elapsed());
 }
 
-// fn draw(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, x: i32, y: i32, w: i32, h: i32) {
+// fn draw(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, x: f32, y: f32, w: f32, h: f32) {
 //     // left to right bool
 //     let left_to_right = rand::random::<f32>();
-//     if x + w < ib.width() as i32 && y + h < ib.height() as i32 {
+//     if x + w < ib.width() as f32 && y + h < ib.height() as f32 {
 //         if left_to_right >= 0.5 {
 //             utils::line(ib, &Point2::new(x, y), &Point2::new(x + w, y + h));
 //         } else {

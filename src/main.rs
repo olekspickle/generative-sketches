2aa25fc1212b1bcb4ff89431bb9c51380bee2f12
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-02-21 22:48:27 +0100

    add base for lines generative exercise

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main(){
-    println!("car not go space, car go road")
+use nannou::prelude::*;
+
+fn main() {
+    nannou::sketch(view).run()
+}
+
+fn view(app: &App, frame: Frame) {
+    // Begin drawing
+    let draw = app.draw();
+
+    // Clear the background to blue.
+    draw.background().color(CORNFLOWERBLUE);
+
+    // Draw a purple triangle in the top left half of the window.
+    let _win = app.window_rect();
+
+    // Draw an ellipse to follow the mouse.
+    let _t = app.time;
+
+    // Draw a quad that follows the inverse of the ellipse.
+    draw.quad()
+        .x_y(app.mouse.x, app.mouse.y)
+        .w_h(500., 500.)
+        // .rotate(t)
+        .color(CORNFLOWERBLUE)
+        .stroke_weight(10.)
+        .stroke_color(MEDIUMSPRINGGREEN);
+
+    // Write the result of our drawing to the window's frame.
+    draw.to_frame(app, &frame).unwrap();
 }

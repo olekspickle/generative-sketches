4c2f835c20e7283ddbd2a666fd6cc9c8c17568c6
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-02-21 22:48:27 +0100

    start to work on joy division example

--- examples/src/lines.rs
+++ examples/src/lines.rs
@@ -1,22 +1,6 @@
 use image::{ImageBuffer, Rgb};
 use std::{path::Path, time::Instant};
-
-struct Context {
-    pub buf: ImageBuffer<Rgb<u8>, Vec<u8>>,
-    pub color: Rgb<u8>,
-}
-
-#[derive(Debug)]
-struct Point2 {
-    x: i32,
-    y: i32,
-}
-
-impl Point2 {
-    fn new(x: i32, y: i32) -> Self {
-        Point2 { x, y }
-    }
-}
+use utils::Point2;
 
 fn main() {
     let start = Instant::now();
@@ -28,7 +12,7 @@ fn main() {
         imgbuf.height(),
         step
     );
-    
+
     // Iterate over the coordinates and pixels of the image
     // to set basic background color
     let scale = 0.55 + (step as f32) / (imgbuf.width() + imgbuf.height()) as f32;
@@ -51,7 +35,7 @@ fn main() {
         .step_by(step as usize)
         .collect();
 
-    // Dedup vector of y, because the image iterator takes pixel/per iteration    
+    // Dedup vector of y, because the image iterator takes pixel/per iteration
     vert.dedup_by(|a, b| a == b);
     let zipped = horiz.iter().zip(vert.iter());
 
@@ -72,79 +56,9 @@ fn draw(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, x: i32, y: i32, w: i32, h: i32) 
     let left_to_right = rand::random::<f32>();
     if x + w < ib.width() as i32 && y + h < ib.height() as i32 {
         if left_to_right >= 0.5 {
-            line(ib, Point2::new(x, y), Point2::new(x + w, y + h));
+            utils::line(ib, Point2::new(x, y), Point2::new(x + w, y + h));
         } else {
-            line(ib, Point2::new(x + w, y), Point2::new(x, y + h));
+            utils::line(ib, Point2::new(x + w, y), Point2::new(x, y + h));
         }
     }
-}
-
-// simplified full Bresenham line algorhitm implementation
-//
-// https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
-fn line(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, p1: Point2, p2: Point2) {
-    if (p2.y - p1.y) < (p2.x - p1.x).abs() {
-        if p1.x > p2.x {
-            plot_line_low(ib, p2, p1)
-        } else {
-            plot_line_low(ib, p1, p2)
-        }
-    } else {
-        if p1.y > p2.y {
-            plot_line_high(ib, p2, p1)
-        } else {
-            plot_line_high(ib, p1, p2)
-        }
-    }
-}
-
-fn plot_line_high(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, p1: Point2, p2: Point2) {
-    let mut dx = p2.x - p1.x;
-    let dy = p2.y - p1.y;
-    let mut xi = 1;
-    if dx < 0 {
-        xi = -1;
-        dx = -dx;
-    }
-    let mut D = 2 * dx - dy;
-    let mut x = p1.x;
-
-    for y in p1.y..p2.y {
-        assign_pixel(ib, x, y);
-
-        if D > 0 {
-            x = x + xi;
-            D = D - 2 * dy;
-        }
-        D = D + 2 * dx
-    }
-}
-
-fn plot_line_low(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, p1: Point2, p2: Point2) {
-    let dx = p2.x - p1.x;
-    let mut dy = p2.y - p1.y;
-    let mut yi = 1;
-    if dy < 0 {
-        yi = -1;
-        dy = -dy;
-    }
-    let mut D = 2 * dy - dx;
-    let mut y = p1.y;
-
-    for x in p1.x..p2.x {
-        assign_pixel(ib, x, y);
-
-        if D > 0 {
-            y = y + yi;
-            D = D - 2 * dx;
-        }
-        D = D + 2 * dy
-    }
-}
-
-fn assign_pixel(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, x: i32, y: i32) {
-    // Mutating single pixel
-    let pixel = ib.get_pixel_mut(x as u32, y as u32);
-    // let data = (*pixel as Rgb<u8>).0;
-    *pixel = Rgb([0.0 as u8, 0.0 as u8, 0.0 as u8]);
 }
--- examples/src/utils.rs
+++ examples/src/utils.rs
@@ -2,6 +2,28 @@ use image::{self, ImageBuffer, ImageFormat, Rgb};
 use std::path::Path;
 use termion::{color, style};
 
+#[derive(Debug)]
+pub struct Point2 {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Point2 {
+    pub fn new(x: i32, y: i32) -> Self {
+        Point2 { x, y }
+    }
+}
+
+#[derive(Debug)]
+pub struct Line {
+    pub points: Vec<Point2>,
+}
+impl Line {
+    pub fn new(points: Vec<Point2>) -> Self {
+        Line { points }
+    }
+}
+
 pub fn save_image(img: ImageBuffer<Rgb<u8>, Vec<u8>>, p: &Path) {
     print_italic(&format!("saving as {:?}...", p));
 
@@ -24,4 +46,74 @@ pub fn print_green(s: &str) {
 
 pub fn print_italic(s: &str) {
     println!("{}{}{}", style::Italic, s, style::Reset);
+}
+
+// simplified full Bresenham line algorhitm implementation
+//
+// https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+pub fn line(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, p1: &Point2, p2: &Point2) {
+    if (p2.y - p1.y) < (p2.x - p1.x).abs() {
+        if p1.x > p2.x {
+            plot_line_low(ib, p2, p1)
+        } else {
+            plot_line_low(ib, p1, p2)
+        }
+    } else {
+        if p1.y > p2.y {
+            plot_line_high(ib, p2, p1)
+        } else {
+            plot_line_high(ib, p1, p2)
+        }
+    }
+}
+
+pub fn plot_line_high(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, p1: &Point2, p2: &Point2) {
+    let mut dx = p2.x - p1.x;
+    let dy = p2.y - p1.y;
+    let mut xi = 1;
+    if dx < 0 {
+        xi = -1;
+        dx = -dx;
+    }
+    let mut d = 2 * dx - dy;
+    let mut x = p1.x;
+
+    for y in p1.y..p2.y {
+        assign_pixel(ib, x, y);
+
+        if d > 0 {
+            x = x + xi;
+            d = d - 2 * dy;
+        }
+        d = d + 2 * dx
+    }
+}
+
+pub fn plot_line_low(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, p1: &Point2, p2: &Point2) {
+    let dx = p2.x - p1.x;
+    let mut dy = p2.y - p1.y;
+    let mut yi = 1;
+    if dy < 0 {
+        yi = -1;
+        dy = -dy;
+    }
+    let mut d = 2 * dy - dx;
+    let mut y = p1.y;
+
+    for x in p1.x..p2.x {
+        assign_pixel(ib, x, y);
+
+        if d > 0 {
+            y = y + yi;
+            d = d - 2 * dx;
+        }
+        d = d + 2 * dy
+    }
+}
+
+pub fn assign_pixel(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, x: i32, y: i32) {
+    // Mutating single pixel
+    let pixel = ib.get_pixel_mut(x as u32, y as u32);
+    // let data = (*pixel as Rgb<u8>).0;
+    *pixel = Rgb([0.0 as u8, 0.0 as u8, 0.0 as u8]);
 }

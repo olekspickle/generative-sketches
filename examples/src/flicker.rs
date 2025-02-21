faf51f6b7912f0a5768b79808cc5007968ff400b
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-02-21 22:48:27 +0100

    update flicker example

--- examples/src/flicker.rs
+++ examples/src/flicker.rs
@@ -18,12 +18,23 @@ fn view(app: &App, frame: Frame) {
 
     // Draw a quad that follows the inverse of the ellipse.
     draw.quad()
-        .x_y(app.mouse.x * t.cos() * PI, app.mouse.y * t.sin())
+        // .x_y(app.mouse.x * t.cos() * PI, app.mouse.y * t.sin())
+        .x_y(app.mouse.x, app.mouse.y)
         .w_h(500., 500.)
-        // .rotate(t)
+        .rotate(t)
         .color(CORNFLOWERBLUE)
         .stroke_weight(5.)
         .stroke_color(MEDIUMSPRINGGREEN);
+
+    for i in 0..50 {
+        draw.line().weight(2.).color(MEDIUMSPRINGGREEN).points(
+            Point2::new(app.mouse.x, app.mouse.y),
+            Point2::new(
+                app.mouse.x + i as f32 * rand::random::<f32>() * 60. * PI,
+                app.mouse.y + i as f32 * rand::random::<f32>() * 60. * PI,
+            ),
+        );
+    }
 
     // Write the result of our drawing to the window's frame.
     draw.to_frame(app, &frame).unwrap();

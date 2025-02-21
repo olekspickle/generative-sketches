13938251b517ff0b18467b80ce8ab81cefa03646
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-02-21 22:48:27 +0100

    draw details: change base type i32 -> f32

--- examples/src/utils.rs
+++ examples/src/utils.rs
@@ -1,15 +1,16 @@
 use image::{self, ImageBuffer, ImageFormat, Rgb};
 use std::path::Path;
 use termion::{color, style};
+use itertools_num::linspace;
 
 #[derive(Debug)]
 pub struct Point2 {
-    pub x: i32,
-    pub y: i32,
+    pub x: f32,
+    pub y: f32,
 }
 
 impl Point2 {
-    pub fn new(x: i32, y: i32) -> Self {
+    pub fn new(x: f32, y: f32) -> Self {
         Point2 { x, y }
     }
 }
@@ -74,87 +75,56 @@ pub fn line(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, p1: &Point2, p2: &Point2) {
 pub fn plot_line_high(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, p1: &Point2, p2: &Point2) {
     let mut dx = p2.x - p1.x;
     let dy = p2.y - p1.y;
-    let mut xi = 1;
-    if dx < 0 {
-        xi = -1;
+    let mut xi = 1.;
+    if dx < 0. {
+        xi = -1.;
         dx = -dx;
     }
-    let mut d = 2 * dx - dy;
+    let mut d = 2. * dx - dy;
     let mut x = p1.x;
-
-    for y in p1.y..p2.y {
+    for y in linspace::<f32>(p1.y, p2.y, 10) {
         assign_pixel(ib, x, y);
 
-        if d > 0 {
+        if d > 0. {
             x = x + xi;
-            d = d - 2 * dy;
+            d = d - 2. * dy;
         }
-        d = d + 2 * dx
+        d = d + 2. * dx
     }
 }
 
 pub fn plot_line_low(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, p1: &Point2, p2: &Point2) {
     let dx = p2.x - p1.x;
     let mut dy = p2.y - p1.y;
-    let mut yi = 1;
-    if dy < 0 {
-        yi = -1;
+    let mut yi = 1.;
+    if dy < 0. {
+        yi = -1.;
         dy = -dy;
     }
-    let mut d = 2 * dy - dx;
+    let mut d = 2. * dy - dx;
     let mut y = p1.y;
 
-    for x in p1.x..p2.x {
+    for x in linspace(p1.x, p2.x, 10) {
         assign_pixel(ib, x, y);
 
-        if d > 0 {
+        if d > 0. {
             y = y + yi;
-            d = d - 2 * dx;
+            d = d - 2. * dx;
         }
-        d = d + 2 * dy
+        d = d + 2. * dy
     }
 }
 
-pub fn plot_line_vertical(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, p1: &Point2, p2: &Point2) {
-    let mut dx = 0;
-    let dy = p2.y - p1.y;
-    let mut xi = 1;
-    let mut d = 2 * dx - dy;
-    let mut x = p1.x;
-
-    for y in p1.y..p2.y {
-        assign_pixel(ib, x, y);
-
-        if d > 0 {
-            x = x + xi;
-            d = d - 2 * dy;
-        }
-        d = d + 2 * dx
-    }
-}
-
-pub fn plot_line_horizontal(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, p1: &Point2, p2: &Point2) {
-    let dx = p2.x - p1.x;
-    let mut dy = 0;
-    let mut yi = 1;
-    let mut d = 2 * dy - dx;
-    let mut y = p1.y;
-
-    for x in p1.x..p2.x {
-        assign_pixel(ib, x, y);
-
-        if d > 0 {
-            y = y + yi;
-            d = d - 2 * dx;
-        }
-        d = d + 2 * dy
-    }
-}
-
-pub fn assign_pixel(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, x: i32, y: i32) {
+pub fn assign_pixel(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, x: f32, y: f32) {
     // Mutating single pixel
     // println!("x:{}, y:{}", x, y);
     let pixel = ib.get_pixel_mut(x as u32, y as u32);
     // let data = (*pixel as Rgb<u8>).0;
     *pixel = Rgb([0.0 as u8, 0.0 as u8, 0.0 as u8]);
 }
+
+// /// Xiaolin Wu`s naive implementation of line drawing with antialiasing 
+// pub fn line_antialised(ib: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, p1: &Point2, p2: &Point2) {
+//     let steep = (p2.y - p1.x).abs() > (p2.x - p1.x).abs();
+    
+// }
